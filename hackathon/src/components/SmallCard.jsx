adeb22e75bff821ef98079af02d5b36024df31d9
yellowmothus <christopherneves04@gmail.com>
2025-07-16 15:31:27 +0100

    small card component, sidebar component

---
+++ hackathon/src/components/SmallCard.jsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import '../styles/SmallCard.css'
+
+export default function SmallCard(props) {
+  
+    return (
+        <div class="card">
+            <h3 class="card-title">
+             Título
+            </h3>
+            <p class="card-value">
+            Valor
+            </p>
+        </div>
+      ) 
+}
