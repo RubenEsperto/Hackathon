f8ddd87cc0e788bbe6e420f58451e86bc0311272
Bytes4Future <bytes4future@AKFPTSNTBY03388.local>
2025-07-16 17:38:44 +0100

    frontend - Added routers

--- hackathon/src/App.js
+++ hackathon/src/App.js
@@ -5,81 +5,65 @@ import Drawer from '@mui/material/Drawer';
 import CssBaseline from '@mui/material/CssBaseline';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
 import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
-import SmallCard from './components/SmallCard.jsx';
-import FoodTable from './components/FeedTable.jsx';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import SidebarMenu from './components/SideBarMenu';
+import ListadeAnimais from './pages/StockAnimals';
+import FoodStock from './pages/StockFood';
 
 const drawerWidth = 240;
 
 function App() {
   return (
-    <div className="App">
-    <div>
-    <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
-      <AppBar
-        position="fixed"
-        sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}
-      >
-        <Toolbar>
-          <Typography variant="h6" noWrap component="div">
-            Olá, username
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          '& .MuiDrawer-paper': {
+    <Router>
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+
+        <AppBar
+          position="fixed"
+          sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}
+        >
+          <Toolbar>
+            <Typography variant="h6" noWrap component="div">
+              Olá, admin! Bem-vindo ao painel de controle.
+            </Typography>
+          </Toolbar>
+        </AppBar>
+
+        <Drawer
+          sx={{
             width: drawerWidth,
-            boxSizing: 'border-box',
-          },
-        }}
-        variant="permanent"
-        anchor="left"
-      >
-        <Toolbar />
-        <Divider />
-        <List>
-          {['Dashboard', 'Animais', 'Stock'].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-        <Divider />
+            flexShrink: 0,
+            '& .MuiDrawer-paper': {
+              width: drawerWidth,
+              boxSizing: 'border-box',
+            },
+          }}
+          variant="permanent"
+          anchor="left"
+        >
+          <Toolbar />
+          <Divider />
+          <SidebarMenu />
+          <Divider />
         </Drawer>
+
+        <Box
+          component="main"
+          sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}
+        >
+          <Toolbar />
+
+          <Routes>
+            <Route path="/stock/animais" element={<ListadeAnimais />} />
+            <Route path="/stock/comida" element={<FoodStock />} />
+            <Route path="/dashboard" element={<div>Dashboard</div>} />
+          </Routes>
+        </Box>
       </Box>
-{/*MAIN CONTENT*/}
-    <Box
-        component="main"
-        sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}>
-        <Toolbar /> 
-        <div class="small-card-wrap">
-          <SmallCard titulo="Estoque Total"  valor="1000(ton)"/>
-          <SmallCard titulo="Estoque Gasto este mês"  valor="1000(ton)"/>
-          <SmallCard titulo="Estoque Sobra"  valor="1000(ton)"/>
-          <SmallCard titulo="Próxima Encomenda"  valor="3 meses" quantia="(300ton/sardinhas)"/>
-          <SmallCard titulo="Última Encomenda"  valor="2 semanas"  quantia="(200ton/crustaceos)"/>
-        </div>
-      </Box>
-     <FoodTable/> 
-     </div>
-     </div>
+    </Router>
   );
 }
 
---
+++ hackathon/src/pages/StockAnimals.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import DataTable from '../components/Table-Animals.jsx';
+
+const ListadeAnimais = () => {
+  return (
+    <div>
+      <h1>Lista de Animais</h1>
+      <DataTable/>
+    </div>
+  );
+};
+
+export default ListadeAnimais;
