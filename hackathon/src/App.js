523a71dee0a246df8baefef1788788029cf94370
yellowmothus <christopherneves04@gmail.com>
2025-07-16 16:06:19 +0100

    small card fix & styling, component separation

--- hackathon/src/App.js
+++ hackathon/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 import PermanentDrawerLeft from './components/sidebar';
+import SmallCard from './components/SmallCard.jsx';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+
 function App() {
   return (
     <div className="App">
      <PermanentDrawerLeft/>
+     
     </div>
   );
 }
